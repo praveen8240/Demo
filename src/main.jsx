4c9919e0efce652ceeecdcc4d0f7a05395aff641
praveen8240 <156305741+praveen8240@users.noreply.github.com>
2025-10-16 14:45:14 +0530

    new version

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import './index.css'
 import Shared1 from './Shared1.jsx'
 import Shared2 from './Shared2.jsx'
 import { TodoProvider } from './context/TodoContext.jsx'
+import GetTodos from './GetTodos.jsx'
 createRoot(document.getElementById('root')).render(
     <TodoProvider>
-        Version11
-    <Shared1/><Shared2/>
+        <GetTodos/>
     </TodoProvider>
 )

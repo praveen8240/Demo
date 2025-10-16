ac134c1cdd5d024c3ffacebcb2baa92f269175cf
praveen8240 <156305741+praveen8240@users.noreply.github.com>
2025-10-16 11:43:04 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Shared1 from './Shared1.jsx'
+import Shared2 from './Shared2.jsx'
+import { TodoProvider } from './context/TodoContext.jsx'
+createRoot(document.getElementById('root')).render(
+    <TodoProvider>
+    <Shared1/><Shared2/>
+    </TodoProvider>
+)
